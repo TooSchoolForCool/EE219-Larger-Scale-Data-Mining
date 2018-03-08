65436ebc3398324c57c3547307dde4896996818d
Zeyu Zhang <zeyuz@outlook.com>
2018-03-08 12:48:52 -0800

    Add preprocessor

---
+++ Project-5/src/preprocess.py
@@ -0,0 +1,42 @@
+import json
+
+TWEET_DATA_PREFIX = "../tweet_data/"
+HASH_TAGS = [
+    "gohawks",
+    "gopatriots",
+    "nfl",
+    "patriots",
+    "sb49",
+    "superbowl"
+]
+
+
+def pre_parse(hash_tag):
+    src_path = TWEET_DATA_PREFIX + "tweets_#" + hash_tag + ".txt"
+    dst_path = TWEET_DATA_PREFIX + hash_tag + ".txt"
+
+    tweets_info = []
+
+    with open(src_path) as src_file:
+        for line in src_file:
+            tweet = json.loads(line)
+
+            target_info = {
+                "date" : tweet["citation_date"],
+                "n_reweets" : tweet["metrics"]["citations"]["total"],
+                "n_followers" : tweet["author"]["followers"]
+            }
+
+            tweets_info.append(target_info)
+
+    with open(dst_path, "w") as dst_file:
+        json.dump(tweets_info, dst_file)
+
+
+def main():
+    for hash_tag in HASH_TAGS:
+        pre_parse(hash_tag)
+
+
+if __name__ == '__main__':
+    main()
